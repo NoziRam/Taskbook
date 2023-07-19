1f35ed1aefb46d2d0dc05d59547fa0c84c2bf5a3
NoziRam <1000dreams000@gmail.com>
2023-07-19 18:52:07 +0500

    Added new classes Begin.cs

---
+++ Abramyan/Begin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abramyan
+{
+     public static class Begin
+    {
+
+        public static void Get()
+        {
+            // Begin-1 Дана сторона квадрата a. Найти его периметр P = 4·a.
+            Console.WriteLine("please enter the number");
+            double a = Convert.ToInt32(Console.ReadLine());
+            double s = 2;
+            double d = Math.Pow(a, s);
+            //double d = a * a;
+            Console.WriteLine(d);
+
+
+        }
+
+
+    }
+}
