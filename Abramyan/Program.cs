dea134dc3fd3b88de0032592aa1ec83f3bd1c0c1
NoziRam <1000dreams000@gmail.com>
2023-07-19 18:52:07 +0500

    Added new classes Integer.cs

---
+++ Abramyan/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Abramyan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Begin.Get();
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
